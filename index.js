e6526637354653bbfd0991abec60d6f089a0d237
ZeyaRabani <ziyarabani@hotmail.co.uk>
2021-04-16 12:41:33 +0100

    installarions

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app =  express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet')
+const morgan = require('morgan');
+
+
+dotenv.config();
+
+
+app.listen(3000, ()=>{
+    console.log("backend server")
+})
